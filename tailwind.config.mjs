195759a41b84760383a920644702098a1613a609
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-12-29 13:58:33 +0530

    feat: Make splash section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,16 @@ export default {
       fontFamily: {
         handwriting: ["Excalifont", "sans-serif"],
       },
+      colors: {
+        background: "#fbf1c7",
+        foreground: "#1d2021",
+        purple: "#d3869b",
+        blue: "#83a598",
+        aqua: "#8ec07c",
+        yellow: "#fabd2f",
+        green: "#b8bb26",
+        red: "#fb4934",
+      },
     },
   },
   plugins: [],
